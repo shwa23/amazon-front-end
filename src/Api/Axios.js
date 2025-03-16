8bd2a7f481d6ee7b80a52e5ac8195fc5f73859f8
shwa23 <shwaatsbeha123@gmail.com>
2025-03-16 02:04:57 -0700

    finished front end

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
     // local version
     // baseURL: "http://127.0.0.1:5001/clone-8c419/us-central1/api",
     // deployed version
-    baseURL:"http://127.0.0.1:5001/fir-8be4d/us-central1/api"
+    baseURL:"https://amazon-backend-eh14.onrender.com/"
 });
 
 export { axiosInstance };
